32ee752c34bc36aae57fb08f96cae560897cdc43
Alberto Bregliano <alberto.bregliano@pm.me>
2019-12-08 23:38:57 +0100

    licensed

--- token/types.go
+++ token/types.go
@@ -1,3 +1,7 @@
+// Copyright 2019 Alberto Bregliano. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package token
 
 // Credentials is the type used to pass username and password around.
